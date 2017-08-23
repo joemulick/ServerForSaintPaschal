d25a3f1916b99d11c03db0a6eeacd4dd0d78851c
Joe Mulick <joemulick@gmail.com>
2017-08-23 01:48:31 -0700

    tutorial 100% accurate and complete through export into static html app end, ready to incorporate actual saint paschal website.

---
+++ out/_next/308814ee-02c3-4d62-99de-1128ba5ec14e/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([6],{347:function(e,t,u){e.exports=u(348)},348:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(8),a=n(l),o=u(114),r=n(o);t.default=function(){return a.default.createElement(r.default,null,a.default.createElement("p",null,"This is the about page"))}}},[347]);
+            return { page: comp.default }
+          })
+        
