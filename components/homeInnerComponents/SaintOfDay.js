5b15e2c187a567b9fd8c84d4c1d25f68c51c752b
Joe Mulick <joemulick@gmail.com>
2017-08-26 12:51:56 -0700

    hardcoded in 5 saints

--- components/homeInnerComponents/SaintOfDay.js
+++ components/homeInnerComponents/SaintOfDay.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
 
 const saints = {
+
+	[
+	{
+
 	name : 'St. Francis of Assisi',
+	image: 'https://res.cloudinary.com/ddsihrmda/image/upload/v1503775501/st-francis-of-assisi_euu2xw.jpg'
 	birthyear : '1181',
-	url : '',
-	youtubeurl : '',
-	bio: 'Founder of the Franciscan Order, born at Assisi in Umbria, in 1181. In 1182, Pietro Bernardone returned from a trip to France to find out his wife had given birth to a son. Far from being excited or apologetic because he\'d been gone, Pietro was furious because she\'d had his new son baptized Giovanni after John the Baptist. The last thing Pietro wanted in his son was a man of God -- he wanted a man of business, a cloth merchant like he was, and he especially wanted a son who would reflect his infatuation with France. So he renamed his son Francesco -- which is the equivalent of calling him Frenchman. Francis enjoyed a very rich easy life growing up because of his father\'s wealth and the permissiveness of the times. From the beginning everyone -- and I mean everyone -- loved Francis. He was constantly happy, charming, and a born leader. If he was picky, people excused him. If he was ill, people took care of him. If he was so much of a dreamer he did poorly in school, no one minded. In many ways he was too easy to like for his own good. No one tried to control him or teach him. As he grew up, Francis became the leader of a crowd of young people who spent their nights in wild parties. Thomas of Celano, his biographer who knew him well, said, "In other respects an exquisite youth, he attracted to himself a whole retinue of young people addicted to evil and accustomed to vice." Francis himself said, "I lived in sin" during that time. Francis fulfilled every hope of Pietro\'s -- even falling in love with France. He loved the songs of France, the romance of France, and especially the free adventurous troubadours of France who wandered through Europe. And despite his dreaming, Francis was also good at business. But Francis wanted more..more than wealth. But not holiness! Francis wanted to be a noble, a knight. Battle was the best place to win the glory and prestige he longed for. He got his first chance when Assisi declared war on their longtime enemy, the nearby town of Perugia.'
+	url : 'https://www.britannica.com/biography/Saint-Francis-of-Assisi',
+	youtubeurl : 'https://www.youtube.com/watch?v=qYynL7fH0P8',
+	bio: 'Saint Francis of Assisi, Italian San Francesco d’Assisi, baptized Giovanni, renamed Francesco, original name Francesco di Pietro di Bernardone (born 1181/82, Assisi, duchy of Spoleto [Italy]—died October 3, 1226, Assisi; canonized July 16, 1228; feast day October 4) founder of the Franciscan orders of the Friars Minor (Ordo Fratrum Minorum), the women’s Order of St. Clare (the Poor Clares), and the lay Third Order. He was also a leader of the movement of evangelical poverty in the early 13th century. His evangelical zeal, consecration to poverty, charity, and personal charisma drew thousands of followers. Francis’s devotion to the human Jesus and his desire to follow Jesus’ example reflected and reinforced important developments in medieval spirituality.'
+	},
+	{
+
+	name: 'St. Christopher',
+	image: 'https://res.cloudinary.com/ddsihrmda/image/upload/v1503776295/st-christopher_hwioso.jpg',
+	birthyear: '3rd century',
+	url: 'https://www.britannica.com/biography/Saint-Christopher',
+	youtubeurl: 'https://www.youtube.com/watch?v=cXI9z6Iu22A',
+	bio: 'Saint Christopher, (flourished 3rd century; Western feast day July 25; Eastern feast day May 9), patron saint of travelers and, in the 20th century, of motorists, one of the Fourteen Holy Helpers. Though one of the most popular saints, there is no certainty that he existed historically. According to the Roman martyrology, he died in Lycia under the Roman emperor Decius (c. 250). He is the hero of many later legends, which represent him as a giant who, after being converted, devoted his life to carrying travelers across a river.'
+
+
+	},{
+
+	name: 'St. Teresa of Avila',
+	image: 'https://res.cloudinary.com/ddsihrmda/image/upload/v1503776409/st-teresa-of-avila_zs3kuq.jpg',
+	birthyear: '1515',
+	url: 'https://www.britannica.com/biography/Saint-Teresa-of-Avila',
+	youtubeurl: 'https://www.youtube.com/watch?v=o-ZqQD4F-4s',
+	bio: 'Saint Teresa of Ávila, also called Saint Teresa of Jesus, original name Teresa de Cepeda y Ahumada (born March 28, 1515, Ávila, Spain—died October 4, 1582, Alba de Tormes; canonized 1622; feast day October 15), Spanish nun, one of the great mystics and religious women of the Roman Catholic church, and author of spiritual classics. She was the originator of the Carmelite Reform, which restored and emphasized the austerity and contemplative character of primitive Carmelite life.'
+
+	},{
+
+	name: 'St. Anthony of Padua',
+	image: 'https://res.cloudinary.com/ddsihrmda/image/upload/v1503776994/st-anthony-of-padua_j1ferb.jpg',
+	birthyear: '1195',
+	url: 'https://www.britannica.com/biography/Saint-Anthony-of-Padua',
+	youtubeurl: 'https://www.youtube.com/watch?v=AAJTsxi6Oj0',
+	bio: 'St. Anthony of Padua, Anthony also spelled Antony, Italian Sant’Antonio da Padova, original name Fernando Martins de Bulhões (born 1195, Lisbon, Portugal—died June 13, 1231, Arcella, Verona [now in Italy]; canonized 1232; feast day June 13), Franciscan friar, doctor of the church, and patron of the poor. Padua and Portugal claim him as their patron saint, and he is invoked for the return of lost property.'
+
+	},{
+
+	name: '',
+	image: '',
+	birthyear: '',
+	url: '',
+	youtubeurl: '',
+	bio: ''
+
+	},{
+
+	name: '',
+	image: '',
+	birthyear: '',
+	url: '',
+	youtubeurl: '',
+	bio: ''
+
+	}
+
+
+	]
+	
 }
 
 class SaintOfDay extends Component {
