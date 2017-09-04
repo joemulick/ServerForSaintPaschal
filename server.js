df937f8976835a52ecefa37d616ebbda1bb89483
Joe Mulick <joemulick@gmail.com>
2017-09-04 15:58:08 -0700

    Added isomorphic-unfetch npm and also changes some minor aesthetic on index.js. Going to rewrite Catholic news page to receive the news articles as props

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.prepare()
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready test on http://localhost:3000')
   })
 })
 .catch((ex) => {
